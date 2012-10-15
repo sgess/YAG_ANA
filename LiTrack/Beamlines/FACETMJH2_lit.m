853360ceaf2c1bb9cf81606ac9c8e93dc115ee3b
sgess <spencergessner@gmail.com>
2012-10-15 04:29:29 -0700

    simple scan

--- LiTrack/Beamlines/FACETMJH2_lit.m
+++ LiTrack/Beamlines/FACETMJH2_lit.m
@@ -65,6 +65,7 @@ comment = 'FACET in Li20';	% text comment which appears at bottom of plots
 % 2.2E10, 40.8, -20.6 gave 22.6kA, 15.4µm with as built LBCCR56 -0.076
 
 Efinal = 20.35; %23
+%Efinal = 23; %23
 T566 = 0.10;
 % contf = 1;
 
@@ -90,6 +91,7 @@ subphase = -21.2;
 ramp = 0;
 RTLR56 = 0.603;
 LBCCR56 = -0.0760;
+%FACETR56 = 0.0036;
 FACETR56 = 0.0040;
 RTLupper = 0.025;
 RTLlower = -0.025;
@@ -144,22 +146,22 @@ thewake = 1;
 z0_bar = z0in_deg*mperdeg;
 
 beamline = [
-      -11		0              0                    0.104969  0		  0        % S-band
+       11		0              0                    0.104969  0		  0        % S-band
        11		compressor     90.                  0.104969  1		  2.13     % Compressor cavity AMPL DR13 13 VDES
-      -26	    RTLlower       RTLupper             0		  0       0        % Approximate energy acceptance of NRTL
+       26	    RTLlower       RTLupper             0		  0       0        % Approximate energy acceptance of NRTL
       -6		RTLR56         1.0535               E0        0		  0        % Design NRTL ~0.603, BDES to KMOD for E-164 gives 0.588
        11		8.444          phase                0.104969  thewake 809.5    % 2-6, nominal 9GeV, no feedback
       -13       9              0.235*3             -90        90      0.104969 % Energy feedback to set 9GeV in chicane
        7	    LBCCR56/2      9.000                0         0       0        % 1st half of the chicane. Design was -0.0745, as built -0.076
-      -7	    LBCCR56/2      9.000                0         0       0        % 2nd half of the chicane. Design was -0.0745, as built -0.076
-       22       5.9E-5         0                    0         0       0        % Approximate SR growth in E-spread from chicane
+       7	    LBCCR56/2      9.000                0         0       0        % 2nd half of the chicane. Design was -0.0745, as built -0.076
+      -22       5.9E-5         0                    0         0       0        % Approximate SR growth in E-spread from chicane
        37		0.01           1                    0		  0		  0        % Clip any rediculously long tails
       -10       Efinal         ramp                 0.104969  thewake 868      % Boost to 23 GeV. 868m w/LCLS-II mods from P. Emma email 4-FEB-2011
-      -6		FACETR56       T566                 Efinal    0		  0        % FACET 'dogleg' like chicane
+       6		FACETR56       T566                 Efinal    0		  0        % FACET 'dogleg' like chicane
        22       0.8E-5         0                    0         0       0        % Approximate SR growth in E-spread from dogleg
        37       0.01           1                    0		  0		  0        % Clip any rediculously long tails
        26       SouthMS        NorthMS              0         0       0        % Momentum Slits in FACET
-      -99	    0              0                    0		  0		  0        % End
+       99	    0              0                    0		  0		  0        % End
        ];
    
 
--- plot_scan.m
+++ plot_scan.m
@@ -1,81 +1,83 @@
 load('scan64.mat');
+save_dir = '/Users/sgess/Desktop/plots/LiTrack/scan64/';
 
-savE = 0;
+savE = 1;
 
 figure(f1);
-contourf(phas(:,1),1000*NAMPL,1000*bl_sig(:,:,2),[0 30 40 50 60 70 80 90 100 120 140 160]);
+contourf(1000*NAMPL,phas(:,1),1000*bl_sig(:,:,2),[0 30 40 50 60 70 80 90 100 120 140 160]);
 colormap(flipud(colormap));
 colorbar;
-xlabel('Equivalent Chirp Phase (Degrees)');
-ylabel('Compressor Amplitude (MV)');
+ylabel('Equivalent Chirp Phase (Degrees)');
+xlabel('Compressor Amplitude (MV)');
 title('Bunch Length at S10 in Microns (Gaussian Sigma)');
-if savE; saveas(gca,'/Users/sgess/Desktop/FACET/PLOTS/scans/bl_sig_S10.pdf'); end;
+if savE; saveas(gca,[save_dir 'bl_sig_S10.pdf']); end;
 
 
 figure(f2);
-contourf(phas(:,1),1000*NAMPL,1000*bl_fwhm(:,:,2),[0 30 40 50 60 70 80 90 100 120 140 160]);
+contourf(1000*NAMPL,phas(:,1),1000*bl_fwhm(:,:,2),[0 30 40 50 60 70 80 90 100 120 140 160]);
 colormap(flipud(colormap));
 colorbar;
-xlabel('Equivalent Chirp Phase (Degrees)');
-ylabel('Compressor Amplitude (MV)');
+ylabel('Equivalent Chirp Phase (Degrees)');
+xlabel('Compressor Amplitude (MV)');
 title('Bunch Length at S10 in Microns (FWHM)');
-if savE; saveas(gca,'/Users/sgess/Desktop/FACET/PLOTS/scans/bl_fwhm_S10.pdf'); end;
+if savE; saveas(gca,[save_dir 'bl_fwhm_S10.pdf']); end;
 
 
 figure(f3);
-contourf(phas(:,1),1000*NAMPL,I_max(:,:,2));
+contourf(1000*NAMPL,phas(:,1),I_max(:,:,2));
 colorbar;
-xlabel('Equivalent Chirp Phase (Degrees)');
-ylabel('Compressor Amplitude (MV)');
+ylabel('Equivalent Chirp Phase (Degrees)');
+xlabel('Compressor Amplitude (MV)');
 title('Peak Current at S10 in kA');
-if savE; saveas(gca,'/Users/sgess/Desktop/FACET/PLOTS/scans/i_max_S10.pdf'); end;
+if savE; saveas(gca,[save_dir 'i_max_S10.pdf']); end;
+
 
 figure(f4);
-contourf(phas(:,1),1000*NAMPL,I_sig(:,:,2));
+contourf(1000*NAMPL,phas(:,1),I_sig(:,:,2));
 colorbar;
-xlabel('Equivalent Chirp Phase (Degrees)');
-ylabel('Compressor Amplitude (MV)');
+ylabel('Equivalent Chirp Phase (Degrees)');
+xlabel('Compressor Amplitude (MV)');
 title('Gaussian Peak Current at S10 in kA');
-if savE; saveas(gca,'/Users/sgess/Desktop/FACET/PLOTS/scans/i_sig_S10.pdf'); end;
-
+if savE; saveas(gca,[save_dir 'i_sig_S10.pdf']); end;
 
 
 
 figure(f5);
-contourf(phas(:,1),1000*NAMPL,1000*bl_sig(:,:,3),[0 10 15 20 25 30 35 40 45 50 60 70 80 90 100]);
+contourf(1000*NAMPL,phas(:,1),1000*bl_sig(:,:,3),[0 10 15 20 25 30 35 40 45 50 60 70 80 90 100]);
 colormap(flipud(colormap));
 colorbar;
-xlabel('Equivalent Chirp Phase (Degrees)');
-ylabel('Compressor Amplitude (MV)');
+ylabel('Equivalent Chirp Phase (Degrees)');
+xlabel('Compressor Amplitude (MV)');
 title('Bunch Length at S20 in Microns (Gaussian Sigma)');
-if savE; saveas(gca,'/Users/sgess/Desktop/FACET/PLOTS/scans/bl_sig_S20.pdf'); end;
+if savE; saveas(gca,[save_dir 'bl_sig_S20.pdf']); end;
 
 
 figure(f6);
-contourf(phas(:,1),1000*NAMPL,1000*bl_fwhm(:,:,3),[0 15 30 45 60 75 90 120 140 160 180 200]);
+contourf(1000*NAMPL,phas(:,1),1000*bl_fwhm(:,:,3),[0 15 30 45 60 75 90 120 140 160 180 200]);
 colormap(flipud(colormap));
 colorbar;
-xlabel('Equivalent Chirp Phase (Degrees)');
-ylabel('Compressor Amplitude (MV)');
+ylabel('Equivalent Chirp Phase (Degrees)');
+xlabel('Compressor Amplitude (MV)');
 title('Bunch Length at S20 in Microns (FWHM)');
-if savE; saveas(gca,'/Users/sgess/Desktop/FACET/PLOTS/scans/bl_fwhm_S20.pdf'); end;
+if savE; saveas(gca,[save_dir 'bl_fwhm_S20.pdf']); end;
 
 
 figure(f7);
-contourf(phas(:,1),1000*NAMPL,I_max(:,:,3),[0 5 10 11 12 13 14 15 16 17 18 19]);
+contourf(1000*NAMPL,phas(:,1),I_max(:,:,3),[0 5 10 11 12 13 14 15 16 17 18 19]);
 colorbar;
-xlabel('Equivalent Chirp Phase (Degrees)');
-ylabel('Compressor Amplitude (MV)');
+ylabel('Equivalent Chirp Phase (Degrees)');
+xlabel('Compressor Amplitude (MV)');
 title('Peak Current at S20 in kA');
-if savE; saveas(gca,'/Users/sgess/Desktop/FACET/PLOTS/scans/i_max_S20.pdf'); end;
+if savE; saveas(gca,[save_dir 'i_max_S20.pdf']); end;
+
 
 figure(f8);
-contourf(phas(:,1),1000*NAMPL,I_sig(:,:,3),[0 5 10 11 12 13 14 15 16 17 18 19]);
+contourf(1000*NAMPL,phas(:,1),I_sig(:,:,3),[0 5 10 11 12 13 14 15 16 17 18 19]);
 colorbar;
-xlabel('Equivalent Chirp Phase (Degrees)');
-ylabel('Compressor Amplitude (MV)');
+ylabel('Equivalent Chirp Phase (Degrees)');
+xlabel('Compressor Amplitude (MV)');
 title('Gaussian Peak Current at S20 in kA');
-if savE; saveas(gca,'/Users/sgess/Desktop/FACET/PLOTS/scans/i_sig_S20.pdf'); end;
+if savE; saveas(gca,[save_dir 'i_sig_S20.pdf']); end;
 
 
 
@@ -94,12 +96,13 @@ title('Bunch Profile in S20 for minimum simulated sigma');
 v = axis;
 text(0.7*v(2),0.95*v(4),['Eq Phase = ' num2str(phas(b(d),d),'%.2f') ' degrees']);
 text(0.7*v(2),0.90*v(4),['Comp Amp = ' num2str(1000*NAMPL(d),'%.2f') ' MV']);
-text(0.7*v(2),0.85*v(4),['Z Sigma = ' num2str(bl_sig(b(d),d,3),'%.3f') ' \mum']);
-text(0.7*v(2),0.80*v(4),['Z FWHM = ' num2str(bl_fwhm(b(d),d,3),'%.3f') ' \mum']);
+text(0.7*v(2),0.85*v(4),['Z Sigma = ' num2str(1000*bl_sig(b(d),d,3),'%.3f') ' \mum']);
+text(0.7*v(2),0.80*v(4),['Z FWHM = ' num2str(1000*bl_fwhm(b(d),d,3),'%.3f') ' \mum']);
 text(0.7*v(2),0.75*v(4),['I peak = ' num2str(I_max(b(d),d,3),'%.3f') ' kA']);
 text(0.7*v(2),0.70*v(4),['I sigma = ' num2str(I_sig(b(d),d,3),'%.3f') ' kA']);
 text(0.7*v(2),0.65*v(4),['E FWHM = ' num2str(e_fwhm(b(d),d,3),'%.3f') ' %']);
-if savE; saveas(gca,'/Users/sgess/Desktop/FACET/PLOTS/scans/minSigBL_profS20.pdf'); end;
+if savE; saveas(gca,[save_dir 'minSigBL_profS20.pdf']); end;
+
 
 figure(f10);
 plot(es(:,b(d),d,3));
@@ -109,13 +112,12 @@ title('Energy Spectrum in S20 for minimum simulated sigma');
 v = axis;
 text(0.7*v(2),0.95*v(4),['Eq Phase = ' num2str(phas(b(d),d),'%.2f') ' degrees']);
 text(0.7*v(2),0.90*v(4),['Comp Amp = ' num2str(1000*NAMPL(d),'%.2f') ' MV']);
-text(0.7*v(2),0.85*v(4),['Z Sigma = ' num2str(bl_sig(b(d),d,3),'%.3f') ' \mum']);
-text(0.7*v(2),0.80*v(4),['Z FWHM = ' num2str(bl_fwhm(b(d),d,3),'%.3f') ' \mum']);
+text(0.7*v(2),0.85*v(4),['Z Sigma = ' num2str(1000*bl_sig(b(d),d,3),'%.3f') ' \mum']);
+text(0.7*v(2),0.80*v(4),['Z FWHM = ' num2str(1000*bl_fwhm(b(d),d,3),'%.3f') ' \mum']);
 text(0.7*v(2),0.75*v(4),['I peak = ' num2str(I_max(b(d),d,3),'%.3f') ' kA']);
 text(0.7*v(2),0.70*v(4),['I sigma = ' num2str(I_sig(b(d),d,3),'%.3f') ' kA']);
 text(0.7*v(2),0.65*v(4),['E FWHM = ' num2str(e_fwhm(b(d),d,3),'%.3f') ' %']);
-if savE; saveas(gca,'/Users/sgess/Desktop/FACET/PLOTS/scans/minSigES_profS20.pdf'); end;
-
+if savE; saveas(gca,[save_dir 'minSigES_profS20.pdf']); end;
 
 
 
@@ -134,12 +136,13 @@ title('Bunch Profile in S20 for minimum simulated FWHM');
 v = axis;
 text(0.7*v(2),0.95*v(4),['Eq Phase = ' num2str(phas(f(h),h),'%.2f') ' degrees']);
 text(0.7*v(2),0.90*v(4),['Comp Amp = ' num2str(1000*NAMPL(h),'%.2f') ' MV']);
-text(0.7*v(2),0.85*v(4),['Sigma = ' num2str(bl_sig(f(h),h,3),'%.2f') ' \mum']); 
-text(0.7*v(2),0.80*v(4),['FWHM = ' num2str(bl_fwhm(f(h),h,3),'%.3f') ' \mum']);
+text(0.7*v(2),0.85*v(4),['Sigma = ' num2str(1000*bl_sig(f(h),h,3),'%.2f') ' \mum']); 
+text(0.7*v(2),0.80*v(4),['FWHM = ' num2str(1000*bl_fwhm(f(h),h,3),'%.3f') ' \mum']);
 text(0.7*v(2),0.75*v(4),['I peak = ' num2str(I_max(f(h),h,3),'%.3f') ' kA']);
 text(0.7*v(2),0.70*v(4),['I sigma = ' num2str(I_sig(f(h),h,3),'%.3f') ' kA']);
 text(0.7*v(2),0.65*v(4),['E FWHM = ' num2str(e_fwhm(f(h),h,3),'%.3f') ' %']);
-if savE; saveas(gca,'/Users/sgess/Desktop/FACET/PLOTS/scans/minFwhmBL_profS20.pdf'); end;
+if savE; saveas(gca,[save_dir 'minFwhmBL_profS20.pdf']); end;
+
 
 figure(f12);
 plot(es(:,f(h),h,3));
@@ -149,12 +152,12 @@ title('Energy Spectrum in S20 for minimum simulated FWHM');
 v = axis;
 text(0.7*v(2),0.95*v(4),['Eq Phase = ' num2str(phas(f(h),h),'%.2f') ' degrees']);
 text(0.7*v(2),0.90*v(4),['Comp Amp = ' num2str(1000*NAMPL(h),'%.2f') ' MV']);
-text(0.7*v(2),0.85*v(4),['Sigma = ' num2str(bl_sig(f(h),h,3),'%.2f') ' \mum']); 
-text(0.7*v(2),0.80*v(4),['FWHM = ' num2str(bl_fwhm(f(h),h,3),'%.3f') ' \mum']);
+text(0.7*v(2),0.85*v(4),['Sigma = ' num2str(1000*bl_sig(f(h),h,3),'%.2f') ' \mum']); 
+text(0.7*v(2),0.80*v(4),['FWHM = ' num2str(1000*bl_fwhm(f(h),h,3),'%.3f') ' \mum']);
 text(0.7*v(2),0.75*v(4),['I peak = ' num2str(I_max(f(h),h,3),'%.3f') ' kA']);
 text(0.7*v(2),0.70*v(4),['I sigma = ' num2str(I_sig(f(h),h,3),'%.3f') ' kA']);
 text(0.7*v(2),0.65*v(4),['E FWHM = ' num2str(e_fwhm(f(h),h,3),'%.3f') ' %']);
-if savE; saveas(gca,'/Users/sgess/Desktop/FACET/PLOTS/scans/minFwhmES_profS20.pdf'); end;
+if savE; saveas(gca,[save_dir 'minFwhmES_profS20.pdf']); end;
 
 
 
@@ -173,12 +176,13 @@ title('Bunch Profile in S20 for maximum simulated Peak Current');
 v = axis;
 text(0.7*v(2),0.95*v(4),['Eq Phase = ' num2str(phas(j(l),l),'%.2f') ' degrees']);
 text(0.7*v(2),0.90*v(4),['Comp Amp = ' num2str(1000*NAMPL(l),'%.2f') ' MV']);
-text(0.7*v(2),0.85*v(4),['Sigma = ' num2str(bl_sig(j(l),l,3),'%.3f') ' \mum']);
-text(0.7*v(2),0.80*v(4),['FWHM = ' num2str(bl_fwhm(j(l),l,3),'%.3f') ' \mum']);
+text(0.7*v(2),0.85*v(4),['Sigma = ' num2str(1000*bl_sig(j(l),l,3),'%.3f') ' \mum']);
+text(0.7*v(2),0.80*v(4),['FWHM = ' num2str(1000*bl_fwhm(j(l),l,3),'%.3f') ' \mum']);
 text(0.7*v(2),0.75*v(4),['I peak = ' num2str(I_max(j(l),l,3),'%.3f') ' kA']);
 text(0.7*v(2),0.70*v(4),['I sigma = ' num2str(I_sig(j(l),l,3),'%.3f') ' kA']);
 text(0.7*v(2),0.65*v(4),['E FWHM = ' num2str(e_fwhm(j(l),l,3),'%.3f') ' %']);
-if savE; saveas(gca,'/Users/sgess/Desktop/FACET/PLOTS/scans/maxPeakIBL_profS20.pdf'); end;
+if savE; saveas(gca,[save_dir 'maxPeakIBL_profS20.pdf']); end;
+
 
 figure(f14);
 plot(es(:,j(l),l,3));
@@ -188,12 +192,12 @@ title('Energy Spectrum in S20 for maximum simulated Peak Current');
 v = axis;
 text(0.7*v(2),0.95*v(4),['Eq Phase = ' num2str(phas(j(l),l),'%.2f') ' degrees']);
 text(0.7*v(2),0.90*v(4),['Comp Amp = ' num2str(1000*NAMPL(l),'%.2f') ' MV']);
-text(0.7*v(2),0.85*v(4),['Sigma = ' num2str(bl_sig(j(l),l,3),'%.3f') ' \mum']);
-text(0.7*v(2),0.80*v(4),['FWHM = ' num2str(bl_fwhm(j(l),l,3),'%.3f') ' \mum']);
+text(0.7*v(2),0.85*v(4),['Sigma = ' num2str(1000*bl_sig(j(l),l,3),'%.3f') ' \mum']);
+text(0.7*v(2),0.80*v(4),['FWHM = ' num2str(1000*bl_fwhm(j(l),l,3),'%.3f') ' \mum']);
 text(0.7*v(2),0.75*v(4),['I peak = ' num2str(I_max(j(l),l,3),'%.3f') ' kA']);
 text(0.7*v(2),0.70*v(4),['I sigma = ' num2str(I_sig(j(l),l,3),'%.3f') ' kA']);
 text(0.7*v(2),0.65*v(4),['E FWHM = ' num2str(e_fwhm(j(l),l,3),'%.3f') ' %']);
-if savE; saveas(gca,'/Users/sgess/Desktop/FACET/PLOTS/scans/maxPeakIES_profS20.pdf'); end;
+if savE; saveas(gca,[save_dir 'maxPeakIES_profS20.pdf']); end;
 
 
 
@@ -213,12 +217,13 @@ title('Bunch Profile in S20 for maximum simulated Gaussian Peak Current');
 v = axis;
 text(0.7*v(2),0.95*v(4),['Eq Phase = ' num2str(phas(n(p),p),'%.2f') ' degrees']);
 text(0.7*v(2),0.90*v(4),['Comp Amp = ' num2str(1000*NAMPL(p),'%.2f') ' MV']);
-text(0.7*v(2),0.85*v(4),['Sigma = ' num2str(bl_sig(n(p),p,3),'%.3f') ' \mum']);
-text(0.7*v(2),0.80*v(4),['FWHM = ' num2str(bl_fwhm(n(p),p,3),'%.3f') ' \mum']);
+text(0.7*v(2),0.85*v(4),['Sigma = ' num2str(1000*bl_sig(n(p),p,3),'%.3f') ' \mum']);
+text(0.7*v(2),0.80*v(4),['FWHM = ' num2str(1000*bl_fwhm(n(p),p,3),'%.3f') ' \mum']);
 text(0.7*v(2),0.75*v(4),['I peak = ' num2str(I_max(n(p),p,3),'%.3f') ' kA']);
 text(0.7*v(2),0.70*v(4),['I sigma = ' num2str(I_sig(n(p),p,3),'%.3f') ' kA']);
 text(0.7*v(2),0.65*v(4),['E FWHM = ' num2str(e_fwhm(n(p),p,3),'%.3f') ' %']);
-if savE; saveas(gca,'/Users/sgess/Desktop/FACET/PLOTS/scans/maxSigIBL_profS20.pdf'); end;
+if savE; saveas(gca,[save_dir 'maxSigIBL_profS20.pdf']); end;
+
 
 figure(f16);
 plot(es(:,n(p),p,3));
@@ -228,9 +233,10 @@ title('Energy Spectrum in S20 for maximum simulated Gaussian Peak Current');
 v = axis;
 text(0.7*v(2),0.95*v(4),['Eq Phase = ' num2str(phas(n(p),p),'%.2f') ' degrees']);
 text(0.7*v(2),0.90*v(4),['Comp Amp = ' num2str(1000*NAMPL(p),'%.2f') ' MV']);
-text(0.7*v(2),0.85*v(4),['Sigma = ' num2str(bl_sig(n(p),p,3),'%.3f') ' \mum']);
-text(0.7*v(2),0.80*v(4),['FWHM = ' num2str(bl_fwhm(n(p),p,3),'%.3f') ' \mum']);
+text(0.7*v(2),0.85*v(4),['Sigma = ' num2str(1000*bl_sig(n(p),p,3),'%.3f') ' \mum']);
+text(0.7*v(2),0.80*v(4),['FWHM = ' num2str(1000*bl_fwhm(n(p),p,3),'%.3f') ' \mum']);
 text(0.7*v(2),0.75*v(4),['I peak = ' num2str(I_max(n(p),p,3),'%.3f') ' kA']);
 text(0.7*v(2),0.70*v(4),['I sigma = ' num2str(I_sig(n(p),p,3),'%.3f') ' kA']);
 text(0.7*v(2),0.65*v(4),['E FWHM = ' num2str(e_fwhm(n(p),p,3),'%.3f') ' %']);
-if savE; saveas(gca,'/Users/sgess/Desktop/FACET/PLOTS/scans/maxSigIES_profS20.pdf'); end;
+if savE; saveas(gca,[save_dir 'maxSigIES_profS20.pdf']); end;
+
