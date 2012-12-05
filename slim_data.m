c1303e5ac5106937db4ee6dbc950371b538e1cc7
sgess <spencergessner@gmail.com>
2012-12-05 15:25:35 -0800

    slim

--- slim_data.m
+++ slim_data.m
@@ -39,26 +39,44 @@ names = fieldnames(data.Sample_01);
 prof = strncmp(fieldnames(data.Sample_01),'PROF',4);
 otrs = strncmp(fieldnames(data.Sample_01),'OTRS',4);
 
+nShot = zeros(1,nSamp);
+for i=1:nSamp;
+    
+    Num = num2str(nSamp,'%02d');
+    %dump profile monitors
+    d1 = rmfield(data.(['Sample_' Num]),names(prof));
+    d1 = rmfield(d1,names(otrs));
+    if exist('d','var')
+        d = [d d1];
+    else
+        d = d1;
+    end
+    
+    nShot(i) = length(d1);
+    
+end
 
-
-%dump profile monitors
-d1 = rmfield(data.Sample_01,names(prof));
-d1 = rmfield(d1,names(otrs));
-d2 = rmfield(data.Sample_02,names(prof));
-d2 = rmfield(d2,names(otrs));
-d3 = rmfield(data.Sample_03,names(prof));
-d3 = rmfield(d3,names(otrs));
-d4 = rmfield(data.Sample_04,names(prof));
-d4 = rmfield(d4,names(otrs));
-d5 = rmfield(data.Sample_05,names(prof));
-d5 = rmfield(d5,names(otrs));
+if 0
+% d2 = rmfield(data.Sample_02,names(prof));
+% d2 = rmfield(d2,names(otrs));
+% d3 = rmfield(data.Sample_03,names(prof));
+% d3 = rmfield(d3,names(otrs));
+% d4 = rmfield(data.Sample_04,names(prof));
+% d4 = rmfield(d4,names(otrs));
+% d5 = rmfield(data.Sample_05,names(prof));
+% d5 = rmfield(d5,names(otrs));
 
 %Make list of events with a YAG image
-nShot = length(d1);
-good_yag  = zeros(nShot,5);
-good_aida = zeros(nShot,5);
-for i=1:nShot
-    good_yag(i,1) = ~isempty(d1(i).YAGS_LI20_2432.prof_pid);
+%nShot = length(d1);
+nMax = max(nShot);
+%good_yag  = zeros(nShot,5);
+%good_aida = zeros(nShot,5);
+good_yag  = zeros(nShot,nMax);
+good_aida = zeros(nShot,nMax);
+
+for j=1:nSamp
+    for i=1:nShot(j)
+        good_yag(i,j) = ~isempty(d(i).YAGS_LI20_2432.prof_pid);
     good_yag(i,2) = ~isempty(d2(i).YAGS_LI20_2432.prof_pid);
     good_yag(i,3) = ~isempty(d3(i).YAGS_LI20_2432.prof_pid);
     good_yag(i,4) = ~isempty(d4(i).YAGS_LI20_2432.prof_pid);
@@ -68,6 +86,7 @@ for i=1:nShot
     good_aida(i,3) = ~isempty(d3(i).aida);
     good_aida(i,4) = ~isempty(d4(i).aida);
     good_aida(i,5) = ~isempty(d5(i).aida);
+    end
 end
 
 %Make sure pids line up between YAG image and Aida
@@ -116,3 +135,4 @@ good_data = [d1((good_yag(:,1) & good_aida(:,1))') d2((good_yag(:,2) & good_aida
     
 
 if savE; save('/Users/sgess/Desktop/data/E200_DATA/E200_1138/E200_1138_Slim.mat','good_data'); end;
+end
