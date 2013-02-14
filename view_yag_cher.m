4d896f911c86080683123c44385cc1ad85ab387f
sgess <spencergessner@gmail.com>
2013-02-14 12:59:25 -0800

    blop

--- view_yag_cher.m
+++ view_yag_cher.m
@@ -1,25 +1,37 @@
 figure(1);
-for i =2:19 
+for i =1:89 
     
-    subplot(2,2,1);
-    yag = rot90(data.Sample_02(i).YAGS_LI20_2432.img',2);
+    if(isempty(good_data(i).YAGS_LI20_2432.img)); continue; end;
+    if(isempty(good_data(i).PROF_LI20_3483.img)); continue; end;
+    if(isempty(good_data(i).PROF_LI20_3485.img)); continue; end;
+    
+    subplot(2,3,1);
+    yag = rot90(good_data(i).YAGS_LI20_2432.img',2);
     imagesc(yag);
     
-    subplot(2,2,2);
-    cer = data.Sample_02(i).PROF_LI20_3483.img(800:1100,600:750)';
+    subplot(2,3,2);
+    cer = good_data(i).PROF_LI20_3483.img(700:1000,150:350);
     imagesc(cer);
     
-    subplot(2,2,3);
-    yag_line = mean(yag(100:125,:));
-    plot(yag_line);
-    axis([0 608 0 100]);
+    subplot(2,3,3);
+    near = good_data(i).PROF_LI20_3485.img(500:800,200:500);
+    imagesc(near);
     
-    subplot(2,2,4);
-    cer_line = mean(cer);
+    subplot(2,3,4);
+    yag_line = mean(yag(150:175,:));
+    plot(yag_line);
+    axis([0 634 0 100]);
+    
+    subplot(2,3,5);
+    cer_line = mean(cer,2);
     plot(cer_line);
-    axis([0 300 0 800]);
+    axis([100 200 100 1000]);
 
-    disp(data.Sample_02(i).aida.klys.phase);
+    subplot(2,3,6);
+    near_line = mean(near,2);
+    plot(near_line);
+    axis([0 300 0 200]);
+    
     pause; 
 
 end
